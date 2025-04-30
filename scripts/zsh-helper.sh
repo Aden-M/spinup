a53b161009ada7607fc88d493f0877a84432ab06
Aden Mann <adenmann@protonmail.com>
2025-04-30 10:15:38 -0500

    Fix ZSH/OMZ Installer & add helper script

---
+++ scripts/zsh-helper.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir ~/zsh-installer
+touch ~/zsh-installer/.zshrc
+touch ~/zsh-installer/adenm.zsh-theme
+touch ~/zsh-installer/zsh.sh
+chmod +x ~/zsh-installer/zsh.sh
--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-sudo apt update && sudo apt install -y zsh
+set -e
 
-chsh -s "$(which zsh)"
-echo "Installed zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installed ohmyzsh"
-cp ./.zshrc ~/.zshrc
-echo "Copied .zshrc"
-
-THEME_NAME="adenm.zsh-theme"
-THEME_PATH="./$THEME_NAME"
+# Resolve the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
-cp "$THEME_PATH" "$ZSH_CUSTOM/themes/"
-echo "Installed custom theme"
+THEME_NAME="adenm.zsh-theme"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/marlonrichert/zsh-autocomplete ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+# Install required packages
+sudo apt update && sudo apt install -y zsh git curl
+
+# Set zsh as the default shell
+chsh -s "$(which zsh)"
+echo "Installed zsh and set as default shell."
+
+# Install Oh My Zsh (non-interactive)
+export RUNZSH=no
+export CHSH=no
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installed Oh My Zsh."
+
+# Copy custom .zshrc
+cp "$SCRIPT_DIR/.zshrc" "$HOME/.zshrc"
+echo "Copied .zshrc to home directory."
+
+# Copy custom theme
+mkdir -p "$ZSH_CUSTOM/themes"
+cp "$SCRIPT_DIR/$THEME_NAME" "$ZSH_CUSTOM/themes/"
+echo "Installed custom theme: $THEME_NAME."
+
+# Install optional plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+git clone https://github.com/marlonrichert/zsh-autocomplete "$ZSH_CUSTOM/plugins/zsh-autocomplete"
+echo "Installed zsh plugins."
+
+echo "Zsh setup complete. Launch a new shell or run 'zsh' to start using it."
